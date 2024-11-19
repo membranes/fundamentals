a139a5d1c8a845cde22bf71c32a1f49feeaa12ba
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-11-19 17:31:10 +0000

    Add src/models/bert/tokenizer.py

---
+++ src/models/bert/tokenizer.py
@@ -0,0 +1,32 @@
+"""Module tokenizer.py"""
+import transformers
+
+import src.elements.arguments as ag
+
+
+class Tokenizer:
+    """
+    Class Tokenizer: <a href="https://arxiv.org/abs/1810.04805" target="_blank">BERT</a>
+     (Bidirectional Encoder Representations from Transformers)
+    """
+
+    def __init__(self, arguments: ag.Arguments):
+        """
+
+        Parameters<br>
+        ------------<br>
+        :param arguments: A suite of values/arguments for machine learning model development.<br>
+        """
+
+        self.__arguments = arguments
+
+    def __call__(self) -> transformers.tokenization_utils_base.PreTrainedTokenizerBase:
+        """
+
+        :return:
+        """
+
+        # Tokenizer
+        return transformers.BertTokenizerFast.from_pretrained(
+            pretrained_model_name_or_path=self.__arguments.pretrained_model_name,
+            clean_up_tokenization_spaces=True)
